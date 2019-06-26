15d806df6b83728bb6b147adfe90023ea79e63ef
Javier Tello <jtello@opengov.com>
2019-06-26 13:26:21 -0400

    pg_dump now only takes data from database

--- pg_dump-to-s3.sh
+++ pg_dump-to-s3.sh
@@ -14,13 +14,13 @@ for db in "${dbs[@]}"; do
     FILENAME="$NOW"-"$db"    
 
     # Dump database
-    pg_dump $PG_DB/$db --format=c  > /tmp/"$FILENAME".dump
+    pg_dump $PG_DB/$db --column-inserts --data-only > /tmp/"$FILENAME".sql
 
     # Copy to S3
-    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp /tmp/"$FILENAME".dump s3://$S3_PATH/"$FILENAME".dump --storage-class STANDARD_IA
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp /tmp/"$FILENAME".sql s3://$S3_PATH/"$FILENAME".sql --storage-class STANDARD_IA
 
     # Delete local file
-    rm /tmp/"$FILENAME".dump
+    rm /tmp/"$FILENAME".sql
 
     # Log
     echo "Database $db is archived"
