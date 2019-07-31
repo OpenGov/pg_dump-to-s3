5ea8c89bd6d0b440aa6ae1b25624f76d6a0dc104
javier-tello23 <49450112+javier-tello23@users.noreply.github.com>
2019-07-31 10:25:53 -0400

    [OD-1344]Added check to only apply deletion logic to files that end in SQL (#3)
    * added check only apply deletion logic to files that end in .sql
    
    * modified message for unsupported file/folder
    
    * removed paranthesis from regex for detecting .sql file extension

--- s3-autodelete.sh
+++ s3-autodelete.sh
@@ -6,28 +6,40 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-date_today=`date`
+dateToday=`date`
+regexEndsInSql='^.*\.sql$'
 
 source $DIR/secrets.conf
 
 # Maximum date (will delete all files older than this date)
-maxDate=`[ "$(uname)" = Linux ] && date --date="${date_today} -${2} day" +%Y-%m-%d" "%T`
+maxDate=`[ "$(uname)" = Linux ] && date --date="${dateToday} -${2} day" +%Y-%m-%d" "%T`
 maxDate=`date -d"$maxDate" +%s`
 
 # Loop thru files
 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 ls s3://$1/ | while read -r line;  do
     # Get file creation date
     createDate=`echo $line|awk {'print $1" "$2'}`
-    createDate=`date -d"$createDate" +%s`
 
-    if [[ $createDate -lt $maxDate ]]
+    if [[ $line =~ $regexEndsInSql ]];
     then
+        createDateCompare=`date -d"$createDate" +%s`
         # Get file name
         fileName=`echo $line|awk {'print $4'}`
-        if [[ $fileName != "" ]]
+
+        if [[ $createDateCompare -lt $maxDate ]];
         then
-            echo "* Delete $fileName";
-            AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  aws s3 rm s3://$1/$fileName
+            if [[ $fileName != "" ]]
+            then
+                echo "* Delete $fileName";
+                AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  aws s3 rm s3://$1/$fileName
+                continue
+            fi
+        else
+            echo "$fileName skipped because it is not older than deletion date: $createDate";
+            continue
         fi
+    else
+      echo "$line skipped because not a suppoorted file or is a folder";
+      continue    
     fi
 done;
